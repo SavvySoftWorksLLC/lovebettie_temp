e57f462ccd775aec662a332c4954fdbaf69cd7ac
Paul Scarrone <paul.scarrone@gmail.com>
2017-05-04 18:16:15 -0400

    Site updated to 38f4352

--- javascripts/all.js
+++ javascripts/all.js
@@ -18842,13 +18842,47 @@ nowIndicatorTimeoutID:null,nowIndicatorIntervalID:null,constructor:function(t,n)
 
 
 
+var modalTemplate = function(context) {
+  return `
+    <div id="${context.id}" class="modal">
+      <div class="modal-content">
+        <h4>${context.header}</h4>
+        <p>${context.body}</p>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+      </div>
+    </div>
+  `
+}
+
 $(document).ready(function() {
+
     var apiKey = jQuery('#calendar').data('apiKey');
     var calendarId = jQuery('#calendar').data('calendarId');
     $('#calendar').fullCalendar({
         googleCalendarApiKey: apiKey,
         events: {
-            googleCalendarId: calendarId
+          googleCalendarId: calendarId
+        },
+        eventRender: function (event, element) {
+          var modalId = `modal-${event.id}`;
+          var modalContext = {
+            id: modalId,
+            header: event.title,
+            body: event.description
+          }
+
+          jQuery('body').append(modalTemplate(modalContext))
+          jQuery(`#${modalId}`).modal()
+
+          element.attr('href', `#${modalId}`);
+          element.click(function() {
+            bootbox.alert({
+              message: 'Description : '+event.description,
+              title: event.title,
+            });
+          });
         }
     });
 });
